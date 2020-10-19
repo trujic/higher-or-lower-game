94ee44a4154c5dfec737cd9784e2f9a643e091ff
trujic <nemanja.trujic@hotmail.com>
2020-10-19 13:15:48 +0200

    Updates

---
+++ precache-manifest.64d49e8250d59628360176472afa6c80.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265feb0ca607ef5d612a3a2eb112ac60",
+    "url": "/trujic/higher-or-lower-game/index.html"
+  },
+  {
+    "revision": "9406e0b7dbea13fcc1f9",
+    "url": "/trujic/higher-or-lower-game/static/css/main.4b0e6fae.chunk.css"
+  },
+  {
+    "revision": "0826eaf1c54ec9983bca",
+    "url": "/trujic/higher-or-lower-game/static/js/2.4ce4070b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trujic/higher-or-lower-game/static/js/2.4ce4070b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9406e0b7dbea13fcc1f9",
+    "url": "/trujic/higher-or-lower-game/static/js/main.90b9c859.chunk.js"
+  },
+  {
+    "revision": "28a89a12aa0bb8b3d73a",
+    "url": "/trujic/higher-or-lower-game/static/js/runtime-main.5c947ef1.js"
+  },
+  {
+    "revision": "ee071552b0e999faa1a237e905edee19",
+    "url": "/trujic/higher-or-lower-game/static/media/1.ee071552.png"
+  },
+  {
+    "revision": "77c92e5baf33f33373078b551598ef09",
+    "url": "/trujic/higher-or-lower-game/static/media/10.77c92e5b.png"
+  },
+  {
+    "revision": "c1b9358c210d761f52b73c57b15f6bf0",
+    "url": "/trujic/higher-or-lower-game/static/media/12.c1b9358c.png"
+  },
+  {
+    "revision": "0672fc9ba7a46861a92e89f162b6636b",
+    "url": "/trujic/higher-or-lower-game/static/media/13.0672fc9b.png"
+  },
+  {
+    "revision": "ce44154d2f82a53faa6ed386e938e9ad",
+    "url": "/trujic/higher-or-lower-game/static/media/14.ce44154d.png"
+  },
+  {
+    "revision": "ec7719ba263d51cb49c45eee5c6007cd",
+    "url": "/trujic/higher-or-lower-game/static/media/2.ec7719ba.png"
+  },
+  {
+    "revision": "4294570c9db8c730042b4d40510f5293",
+    "url": "/trujic/higher-or-lower-game/static/media/3.4294570c.png"
+  },
+  {
+    "revision": "497d5e69fef8a804d2ef056c1e7e996e",
+    "url": "/trujic/higher-or-lower-game/static/media/4.497d5e69.png"
+  },
+  {
+    "revision": "5949deb906b0a0b96b97820cff9d0b02",
+    "url": "/trujic/higher-or-lower-game/static/media/5.5949deb9.png"
+  },
+  {
+    "revision": "93dc24a8aa5696ec39dd064938a0edb5",
+    "url": "/trujic/higher-or-lower-game/static/media/6.93dc24a8.png"
+  },
+  {
+    "revision": "c2e944279f8b6070d0d181a8277e12df",
+    "url": "/trujic/higher-or-lower-game/static/media/7.c2e94427.png"
+  },
+  {
+    "revision": "02a8486337225d738bb5bb86b3a2598b",
+    "url": "/trujic/higher-or-lower-game/static/media/8.02a84863.png"
+  },
+  {
+    "revision": "373336fb710f178566030b286f259c1a",
+    "url": "/trujic/higher-or-lower-game/static/media/9.373336fb.png"
+  }
+]);
